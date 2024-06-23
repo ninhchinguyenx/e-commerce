dd8a9a92902ee9656ada797eb1da7897620a6b6f
Ninh NC <ninhchinguyenx@gmail.com>
2024-06-24 03:33:45 +0700

    feature/table product

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,76 +1,76 @@
 @extends('admin.layouts.master')
 
+@section('style-libs')
+   <!--datatable css-->
+   <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" />
+    <!--datatable responsive css-->
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css" />
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
+@endsection
+
 
 @section('content')
+
 
 <div class="row">
   <div class="col-lg-12">
     <div class="card">
       <div class="card-header">
-        <h5 class="card-title mb-0">List Product</h5>
+        <h5 class="card-title mb-0">List Product </h5>
       </div>
       <div class="card-body">
         <div id="example_wrapper" class="dataTables_wrapper dt-bootstrap5 no-footer">
-          <div class="row">
-            <div class="col-sm-12 col-md-6">
-              <div class="dataTables_length" id="example_length"><label>Show <select name="example_length" aria-controls="example" class="form-select form-select-sm">
-                    <option value="10">10</option>
-                    <option value="25">25</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                  </select> entries</label></div>
-            </div>
-            <div class="col-sm-12 col-md-6">
-              <div id="example_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example"></label></div>
-            </div>
-          </div>
           <div class="row">
             <div class="col-sm-12">
               <table id="example" class="table table-bordered dt-responsive nowrap table-striped align-middle dataTable no-footer dtr-inline collapsed" style="width: 100%;" aria-describedby="example_info">
                 <thead>
                   <tr>
-                    <th scope="col" style="width: 16.8px;" class="sorting sorting_desc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="
+                    <th scope="col" style="width: 17.4px;" class="sorting sorting_asc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="
+                               
                                                     
-                                                        
-                                                    
-                                                : activate to sort column ascending" aria-sort="descending">
+                                                : activate to sort column descending">
                       <div class="form-check">
                         <input class="form-check-input fs-15" type="checkbox" id="checkAll" value="option">
                       </div>
                     </th>
-                    <th >ID</th>
-                    <th >Img Thumnail</th>
-                    <th >Price Regular</th>
-                    <th>Price Sale</th>
-                    <th >SKU</th>
-                    <th>View</th>
-                    <th>Is Active</th>
-                    <th >Is Good Deal</th>
-                    <th >Is Show Home</th>
-                    <th>Is New</th>
-                    <th >Created At</th>
-                    <th>Updated At</th>
-                    <th>Action</th>
+                    <th data-ordering="false" class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 32.4px;" aria-label="ID: activate to sort column ascending">ID</th>
+                    <th data-ordering="false" class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 78.4px;" aria-label="Name: activate to sort column ascending">Name</th>
+                    <th data-ordering="false" class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 262.4px;" aria-label="Image Thumbnail: activate to sort column ascending">Image Thumbnail</th>
+                    <th data-ordering="false" class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 71.4px;" aria-label="Price Regular: activate to sort column ascending">Price Regular</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 80.4px;" aria-label="Price Sale: activate to sort column ascending">Price Sale</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 73.4px;" aria-label="SKU: activate to sort column ascending">SKU</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 73.4px;" aria-label="Material: activate to sort column ascending">Material</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 45.4px;" aria-label="Views: activate to sort column ascending">Views</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 43.4px; display: none;" aria-label="Action: activate to sort column ascending">Action</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 47.4px; display: none;" aria-label="Is Active">Is Active</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 47.4px; display: none;" aria-label="Is Hot Deal">Is Hot Deal</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 47.4px; display: none;" aria-label="Is Show Home">Is Show Home</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 47.4px; display: none;" aria-label="Is Good Deal">Is Good Deal</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 47.4px; display: none;" aria-label="Is New">Is New</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 47.4px; display: none;" aria-label="Text">Text</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 47.4px; display: none;" aria-label="Content">Content</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 47.4px; display: none;" aria-label="Use manual">Use manual</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 80.4px; display: none;" aria-label="Create Date: activate to sort column ascending">Create Date</th>
+                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 80.4px; display: none;" aria-label="Update Date: activate to sort column ascending">Update Date</th>
                   </tr>
                 </thead>
                 <tbody>
-
-                  <tr class="odd">
+                  @foreach($data as $item)
+                  <tr>
                     <th scope="row" class="dtr-control sorting_1" tabindex="0">
-                      <div class="form-check">
+                      <!-- <div class="form-check">
                         <input class="form-check-input fs-15" type="checkbox" name="checkAll" value="option1">
-                      </div>
+                      </div> -->
                     </th>
-                    <td>01</td>
-                    <td>VLZ-452</td>
-                    <td>VLZ1400087402</td>
-                    <td><a href="#!">Post launch reminder/ post list</a></td>
-                    <td>Joseph Parker</td>
-                    <td>Alexis Clarke</td>
-                    <td>Joseph Parker</td>
-                    <td>03 Oct, 2021</td>
-                    <td><span class="badge bg-info-subtle text-info">Re-open</span></td>
-                    <td style="display: none;"><span class="badge bg-danger">High</span></td>
+                    <td>{{$item->id}}</td>
+                    <td>{{$item->name}}</td>
+                    <td style=""><a href="#!"><img src="" alt=""></a></td>
+                    <td style="">{{$item->price_regular}}</td>
+                    <td style="">{{$item->price_sale}}</td>
+                    <td style="">{{$item->sku}}</td>
+                    <td>{{$item->material}} 1</td>
+                    <td>{{$item->views}} </td>
                     <td style="display: none;">
                       <div class="dropdown d-inline-block">
                         <button class="btn btn-soft-secondary btn-sm dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -87,31 +87,45 @@
                         </ul>
                       </div>
                     </td>
-                  </tr>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+                    <td style="display: none;"><span class="badge bg-info-subtle text-info">{{$item->views}}</span></td>
+            
                 
+                  </tr>
+                  @endforeach                       
                 </tbody>
               </table>
             </div>
           </div>
-          <div class="row">
-            <div class="col-sm-12 col-md-5">
-              <div class="dataTables_info" id="example_info" role="status" aria-live="polite">Showing 1 to 10 of 14 entries</div>
-            </div>
-            <div class="col-sm-12 col-md-7">
-              <div class="dataTables_paginate paging_simple_numbers" id="example_paginate">
-                <ul class="pagination">
-                  <li class="paginate_button page-item previous disabled" id="example_previous"><a href="#" aria-controls="example" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
-                  <li class="paginate_button page-item active"><a href="#" aria-controls="example" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
-                  <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
-                  <li class="paginate_button page-item next" id="example_next"><a href="#" aria-controls="example" data-dt-idx="3" tabindex="0" class="page-link">Next</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          
         </div>
       </div>
     </div>
   </div><!--end col-->
 </div>
 
+
+@endsection
+
+@section('script-libs')
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<!--datatable js-->
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="{{asset('theme/admin')}}/assets/js/pages/datatables.init.js"></script>
 @endsection
